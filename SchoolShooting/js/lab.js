a0ae81f841718031d8f69212462892369af73b63
Kelsey Melott <knightsofanubis352@gmail.com>
2023-05-20 10:35:52 -0700

    Storyline complete for the Playable prototype

--- SchoolShooting/js/lab.js
+++ SchoolShooting/js/lab.js
@@ -50,8 +50,9 @@ rooms = {
     silence: {
 	    name: "Baskin Engineering Room 152: Silence your phone",
 	    description: "You quickly silence your phone and gesture to others to do the same. It would be bad to alert to the shooter that people were in the room.",
-	    exitKeys: [],
-	    exitTexts: []
+	    exitKeys: ["footsteps", "remind"],
+	    exitTexts: ["Listen for footsteps.", "Remind others near you to silence their phones."]
+
     },
     call: {
         name: "Baskin Engineering Room 152: Call a friend",
@@ -59,14 +60,84 @@ rooms = {
 	    exitKeys: ["silence"],
 	    exitTexts: ["Please, silence your phone."]
     },
-
-
+    footsteps: {
+        name: "Baskin Engineering Room 152: Listening...",
+        description: "You stay quiet and listen… You hear gunshots somewhere above you and the sound of running in the hallway outside the door. \
+        You think for a moment if you should try to make a run for it while believing the shooter is on the level above you.",
+        exitKeys: ["run2", "stay"], 
+        exitTexts: ["You make your escape.", "You stay hidden."]
+    },
+    remind: {
+        name: "Baskin Engineering Room 152: Reminder...",
+        description: "You whisper to the person next to you, 'hey, silence your phone.' They nod and pass on the message.",
+        exitKeys: ["listen", "weapon" ],
+        exitTexts: ["Listen for footsteps.", "Look around for a weapon."]
+    },
+    listen: {
+        name: "Baskin Engineering Room 152: Listening…",
+        description: "You listen closely, flinching when you hear the sound of gunshots in the hallway outside the room. You can feel your heartbeat \
+         speed up as footsteps get closer. There is a moment when the footsteps stop… light enters the room.",
+        exitKeys: ["stay2", "use", "use"],
+        exitTexts: ["Stay hidden.", "Use weapon.", "Try to grab the shooter"], 
+    },
+    
+    weapon: {
+        name: "Finding a Weapon",
+        description: "You look around and see a metal water bottle. You grab it.",
+        exitKeys:["listen"],
+        exitTexts: ["You listen for footsteps."]
+    },
+    stay2: {
+        name: "Staying hidden. Not the right choice.",
+        description: "There are three main choices when dealing with a school shooter: hide, run, or fight. In this instance you would want to try and \
+        catch the shooter off guard and fight instead of staying hidden as the shooter has already discovered your hiding place.",
+        exitKeys: ["listen"],
+        exitTexts: ["Return to Listening..."]
+    },
+    run2: {
+        name: "Running…",
+        description: "Now that you know the shooter is above you, you make your way to the door and run for the exit. You make it and once outside \
+         you creep off into the forest out of sight of the second floor windows.",
+        exitKeys: ["finish"],
+        exitTexts: ["You hear a siren..."]
+    },
+    stay: {
+        name: "Staying put.",
+        description: "Not wanting to risk it you stay put and wait it out. You don't know how much time has passed while you try to stay quiet and calm. \
+        It isn't till you hear 'The shooter has been neutralized, everyone is safe to leave the building.' that you breathe a sigh of relief.", 
+        exitKeys: ["finish"],
+        exitTexts: ["You are safe."]
+    },
+    
+    
+    use: {
+        name: "Using a weapon", //default is unarmed
+        description: "You leap out and attack the shoot catching them off guard. Around you other students jump up and help disarm the shooter.",
+        exitKeys: ["finish2"],
+        exitTexts: ["It's over..."]
+    },
+    finish: {
+        name: "Police.",
+        description: "You make your way to the police blockade. They ask you if you're okay and what you know about what happened. \
+        They sit you down and let the shock wear off. You are given the numbers of trauma counselors and websites to help deal with what just happened.",
+    //list of helpful resources??? 
+    },
+    
+    finish2: {
+        name: "Police.",
+        description: "While you and other students keep the shoot incapacitated another calls the police. They storm the building and quickly \
+        find your class room. 'We can take it from here.' They say, gesturing for you all to leave.",
+        exitKeys: ["finish"],
+        exitTexts: ["You make your way out..."] 
+    },
+    
     
 }
 
 // global variables
 var currentRoom = 1;
-var lastRoom = 20;
+var lastRoom = 18;
+
 
 
 /*
@@ -95,6 +166,7 @@ function displayCurrentRoom(roomObj) {
     display("<p class='description'>" + roomObj.description + "</p>");
 }
 
+
 /*
  * Display current exitKeys
  * Parameters: Takes a room object
@@ -118,7 +190,9 @@ function newRoom(nextRoom) {
     currentRoomObj = rooms[currentRoom];
     clearDisplayArea();
     displayCurrentRoom(currentRoomObj);
-    displayCurrentExits(currentRoomObj);
+    if(currentRoom != "finish"){
+        displayCurrentExits(currentRoomObj);
+    }
 }
 // we need to wait until the webpage is loaded before we display room 1
 document.addEventListener("DOMContentLoaded", function(event) { 
